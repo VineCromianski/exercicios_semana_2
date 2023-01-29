64dfc70ec8af7e5a086be73bd9b6ddaba1c50264
Vinícius Cromianski <117197953+VineCromianski@users.noreply.github.com>
2023-01-29 10:26:15 -0300

    Add files via upload

---
+++ exercicios_semana_2/exercicios.js
@@ -0,0 +1,29 @@
+//Exercício 2
+
+var nomes = ("João", "Maria", "José");
+let roupas = ["camisa", "boné", "meias"];
+const prefixo = +55;
+let numeral = [1, 2, 3];
+let objetos = { a: 1, b: 2 };
+
+//Exercício 3
+
+let churrasco = ["Carne", "Pão de alho", "Bebidas", "Carvão", "Lenha"];
+
+console.log(churrasco);
+
+//Exercício 4
+
+let idade = 15;
+
+if (idade >= 18) {
+  console.log("Você é maior de idade!");
+} else {
+  console.log("Você é menor de idade!");
+}
+
+//Exercio 5
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
